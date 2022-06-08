237aa81247e05963c0b74c80c21c3df8cfc1790b
Yen <p12427@gmail.com>
2022-06-08 14:13:25 +0800

    feat: set express-handlebars as template engine

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // packages and variables
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
+
+// set template engine: express-handlebars
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 // set router: get homepage
 app.get('/', (req, res) => {
-  res.send('This is homepage')
+  res.render('index')
 })
 
 // start and listen on the express server 
