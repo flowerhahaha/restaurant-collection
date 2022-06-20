250427ac2fc181be146ad19820641dddd534b959
Yen <p12427@gmail.com>
2022-06-20 14:53:24 +0800

    refactor: modify restaurant seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -3,19 +3,8 @@ const restaurantList = require('../../restaurant.json').results
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  restaurantList.forEach(data => {
-    Restaurant.create({
-      name: data.name,
-			nameEn: data['name_en'],
-			category: data.category,
-			image: data.image,
-			location: data.location,
-			phone: data.phone,
-			googleMap: data.google_map,
-			rating: data.rating,
-			description: data.description
-    })
-  })
-  console.log('The restaurant seeder has been created.')
+	Restaurant.create(restaurantList)
+		.then(() => console.log('The restaurant seed has been created.'))
+		.catch(e => console.log(e))
 })
 
