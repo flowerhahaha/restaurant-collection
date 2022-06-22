7e9a382eafc42cffc4979cdd944400f5d6761560
Yen <p12427@gmail.com>
2022-06-23 03:17:48 +0800

    perf: modify search router

--- routes/modules/search.js
+++ routes/modules/search.js
@@ -9,8 +9,12 @@ router.get('/', (req, res) => {
   categoryValue = category === 'All' ? /./ : category
 
   Restaurant
-    .find({ $or: [{ name: regExp }, { location: regExp }] })
-    .find({ category: categoryValue })
+    .find({    
+      $and: [
+        { $or: [{ name: regExp }, { location: regExp }] },
+        { category: categoryValue }
+      ] 
+    })
     .lean()
     .sort(sorting)
     .then(filteredData => res.render('index', { restaurantList: filteredData, keyword, sorting, category }))
