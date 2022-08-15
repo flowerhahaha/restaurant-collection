7aaf6530d24ca55fa8f3bc893fa88d66f564abcf
Yen <p12427@gmail.com>
2022-08-15 17:57:51 +0800

    feat: add user register page and router

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,4 +5,9 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
+// router: get user register page
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
 module.exports = router
