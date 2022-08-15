ac417ae6439cb4dc80c148587886420545d48490
Yen <p12427@gmail.com>
2022-08-15 17:45:44 +0800

    feat: add user login page and router

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const router = require('express').Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
+const users = require('./modules/users')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/search', search)
+router.use('/users', users)
 
 router.get('*', (req, res) => {
   res.status(404)
